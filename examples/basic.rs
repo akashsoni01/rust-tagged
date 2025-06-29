f12642199be5b8c2ce641572067d3091e1614dd8
Akash soni <33283321+akashsoni01@users.noreply.github.com>
2025-06-29 16:04:16 +0530

    publish wip

--- examples/basic.rs
+++
@@ -1,8 +0,0 @@
-
-use tagged_core::Tagged;
-struct UserIdTag;
-
-fn main() {
-    let id = Tagged::<u32, UserIdTag>::new(42);
-    println!("Tagged value is : {}", id.value());
-}
--- src/lib.rs
+++
@@ -1,9 +0,0 @@
-pub mod rust_tagged {
-    pub use tagged_core::*;
-}
-
-/*
-we can also write it
-*/
-
-// pub use tagged_core::*;
