1663754edd008e930e0249ffd90f752c80ba239e
Akash soni <33283321+akashsoni01@users.noreply.github.com>
2025-06-29 10:58:49 +0530

    added examples

--- crates/tagged-core/src/lib.rs
+++ crates/tagged-core/src/lib.rs
@@ -1,3 +1,4 @@
+use std::ops::Deref;
 pub struct Tagged<T, U> {
     value: T,
     _marker: std::marker::PhantomData<U>,
@@ -16,6 +17,36 @@ impl<T, U> Tagged<T, U> {
     }
 }
 
+
+/// Blanket `From<T>` for `Tagged<T, U>`
+impl<T, U> From<T> for Tagged<T, U> {
+    fn from(value: T) -> Self {
+        Tagged::new(value)
+    }
+}
+
+/// Support `From<&str>` → `Tagged<String, U>`
+impl<U> From<&str> for Tagged<String, U> {
+    fn from(s: &str) -> Self {
+        Tagged::new(s.to_string())
+    }
+}
+
+/// Support `From<&String>` → `Tagged<String, U>`
+impl<U> From<&String> for Tagged<String, U> {
+    fn from(s: &String) -> Self {
+        Tagged::new(s.clone())
+    }
+}
+
+impl<T, U> Deref for Tagged<T, U> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.value
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -29,8 +60,8 @@ mod tests {
             id: Tagged::new(0)
         };
 
-        // tagged_strut.id = 3; // 
-        
+        // tagged_strut.id = 3; //
+
         assert_eq!(tagged_struct.id.value, 0);
     }
 }
