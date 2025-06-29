21efc4617aa601addca5a846689cdb4ddc05293e
Akash soni <33283321+akashsoni01@users.noreply.github.com>
2025-06-29 09:53:14 +0530

    example added

--- crates/tagged-core/examples/basic.rs
+++ crates/tagged-core/examples/basic.rs
@@ -1,10 +1,9 @@
-// use tagged_core::{Tagged, Tag};
+use tagged_core::{Tagged, Tag};
 
-// struct UserIdTag;
-// impl Tag for UserIdTag {}
+struct UserIdTag;
+impl Tag for UserIdTag {}
 
 fn main() {
-    // let id = Tagged::<u32, UserIdTag>::new(42);
-    // println!("Tagged value: {}", id.value());
-    println!("jhjh");
+    let id = Tagged::<u32, UserIdTag>::new(42);
+    println!("Tagged value: {}", id.value());
 }
