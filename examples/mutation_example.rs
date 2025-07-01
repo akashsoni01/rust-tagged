3623194e3c859809783dfb26eb87052a0726f13e
Akash soni <33283321+akashsoni01@users.noreply.github.com>
2025-07-02 01:08:44 +0530

    #15 added

---
+++ examples/mutation_example.rs
@@ -0,0 +1,14 @@
+use tagged_core::Tagged;
+
+#[derive(Debug)]
+struct Org;
+
+type OrgName = Tagged<String, Org>;
+
+fn main() {
+    let mut name = OrgName::new("Codefonsi".into());
+
+    name.set("New Org Name".into());
+
+    println!("Updated Org Name: {}", name.value());
+}
