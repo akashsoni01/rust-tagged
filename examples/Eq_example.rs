457e69081d07346842f3335d567e823c98c05d3a
Akash soni <33283321+akashsoni01@users.noreply.github.com>
2025-07-01 23:19:46 +0530

    eq, partialeq, ord and reexported in more ergonomics

---
+++ examples/Eq_example.rs
@@ -0,0 +1,32 @@
+use tagged_core::Tagged;
+
+#[test]
+fn test_eq() {
+    let a = Tagged::<u32, String>::new(42);
+    let b = Tagged::<u32, String>::new(42);
+    let c = Tagged::<u32, String>::new(35);
+    let d = Tagged::<u32, String>::new(0);
+
+    assert!(a == b);
+    assert!(a > c);
+    assert!(a >= d);
+
+}
+
+#[test]
+fn test_with_struct() {
+    use tagged_core::Tagged;
+
+    struct UserIdTag {
+        a: Tagged<u32, Self>,
+        b: Tagged<u32, Self>,
+    }
+
+
+    let instance = UserIdTag{a: 1.into(), b: 2.into()};
+
+    assert!(instance.a < instance.b);
+
+    // println!("{}", a);
+
+}
